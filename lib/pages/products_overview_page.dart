c63f1b7a66d10d6e7300fca939489265461fe007
Danilo Portela <daniloportela97@gmail.com>
2022-12-29 12:00:28 -0300

    Usar provider.

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
-  ProductsOverviewPage({super.key});
+  const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text(
-          'Minha Loja',
-        ),
+        title: const Text('Minha Loja'),
       ),
       body: GridView.builder(
         padding: const EdgeInsets.all(10),
         itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(
-          product: loadedProducts[i],
-        ),
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
