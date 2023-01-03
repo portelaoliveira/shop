e9a740c1cf701b774a4ec24781a1d4730984bdfc
Danilo Portela <daniloportela97@gmail.com>
2023-01-03 14:49:03 -0300

    Campo de entrada de multiplas linhas.

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -9,6 +9,15 @@ class ProductFormPage extends StatefulWidget {
 
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _priceFocus.dispose();
+    _descriptionFocus.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +48,18 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 focusNode: _priceFocus,
                 keyboardType:
                     const TextInputType.numberWithOptions(decimal: true),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocus);
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Descrição',
+                ),
+                textInputAction: TextInputAction.next,
+                focusNode: _descriptionFocus,
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
               ),
             ],
           ),
