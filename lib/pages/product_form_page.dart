c014111ba7d585a896ed9491d38d0badb065f0ed
Danilo Portela <daniloportela97@gmail.com>
2023-01-03 10:38:11 -0300

    Usar o Form do Flutter.

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -15,6 +15,21 @@ class _ProductFormPageState extends State<ProductFormPage> {
         centerTitle: true,
         title: const Text('Formulário de Produto'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
