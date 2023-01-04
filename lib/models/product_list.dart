06404e17f30d5eaf944f46486e5474b98c1fc577
Danilo Portela <daniloportela97@gmail.com>
2023-01-04 15:31:11 -0300

    Excluir produto.

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -45,4 +45,12 @@ class ProductList with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void removeProduct(Product product) {
+    int index = _items.indexWhere((p) => p.id == product.id);
+    if (index >= 0) {
+      _items.removeWhere((p) => p.id == product.id);
+      notifyListeners();
+    }
+  }
 }
