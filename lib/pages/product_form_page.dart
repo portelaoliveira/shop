80db8d8f2d606ac5674b21a08e2732e5bfb21845
Danilo Portela <daniloportela97@gmail.com>
2023-01-05 16:19:22 -0300

    Async/Await.

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -71,7 +71,7 @@ class _ProductFormPageState extends State<ProductFormPage> {
     return isValidUrl && endsWithFile;
   }
 
-  void _submitForm() {
+  Future<void> _submitForm() async {
     final isValid = _formKey.currentState?.validate() ?? false;
 
     if (!isValid) {
@@ -82,31 +82,29 @@ class _ProductFormPageState extends State<ProductFormPage> {
 
     setState(() => _isLoading = true);
 
-    Provider.of<ProductList>(
-      context,
-      listen: false,
-    ).saveProduct(_formData).catchError(
-      (error) {
-        return showDialog<void>(
-          context: context,
-          builder: (ctx) => AlertDialog(
-            title: const Text('Ocorreu um erro!'),
-            content: const Text('Ocorreu um erro para salvar o produto.'),
-            actions: [
-              TextButton(
-                child: Text('Ok'),
-                onPressed: () => Navigator.of(context).pop(),
-              ),
-            ],
-          ),
-        );
-      },
-    ).then(
-      (value) {
-        setState(() => _isLoading = false);
-        Navigator.of(context).pop();
-      },
-    );
+    try {
+      await Provider.of<ProductList>(
+        context,
+        listen: false,
+      ).saveProduct(_formData);
+      Navigator.of(context).pop();
+    } catch (error) {
+      await showDialog<void>(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Ocorreu um erro!'),
+          content: const Text('Ocorreu um erro para salvar o produto.'),
+          actions: [
+            TextButton(
+              child: Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
+      );
+    } finally {
+      setState(() => _isLoading = false);
+    }
   }
 
   @override
