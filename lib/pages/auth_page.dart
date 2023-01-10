587949271b47be41cf9aa83a5f63dc23114169e7
Danilo Portela <daniloportela97@gmail.com>
2023-01-10 11:48:14 -0300

    Tela de Autenticação#02.

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
@@ -19,10 +21,45 @@ class AuthPage extends StatelessWidget {
             ),
           ),
         ),
-        Container(
-          width: double.infinity,
-          child: Column(
-            children: [],
+        Positioned(
+          top: 0,
+          bottom: 350,
+          left: 0,
+          right: 0,
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 70,
+                  ),
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black26,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: const Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ],
