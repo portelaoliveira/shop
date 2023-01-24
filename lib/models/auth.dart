ce490551530f1f4639a095eec321edd21f8d7ba0
Danilo Portela <daniloportela97@gmail.com>
2023-01-24 12:57:13 -0300

    Implementar Logout.

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -7,7 +7,7 @@ import 'package:shop/exceptions/auth_exception.dart';
 class Auth with ChangeNotifier {
   String? _token;
   String? _email;
-  String? _uid;
+  String? _userId;
   DateTime? _expiryDate;
 
   bool get isAuth {
@@ -24,7 +24,7 @@ class Auth with ChangeNotifier {
   }
 
   String? get userId {
-    return isAuth ? _uid : null;
+    return isAuth ? _userId : null;
   }
 
   Future<void> _authenticate(
@@ -46,7 +46,7 @@ class Auth with ChangeNotifier {
     } else {
       _token = body['idToken'];
       _email = body['email'];
-      _uid = body['localId'];
+      _userId = body['localId'];
 
       _expiryDate = DateTime.now().add(
         Duration(
@@ -63,5 +63,13 @@ class Auth with ChangeNotifier {
 
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
+  }
+
+  void logout() {
+    _token = null;
+    _email = null;
+    _userId = null;
+    _expiryDate = null;
+    notifyListeners();
   }
 }
